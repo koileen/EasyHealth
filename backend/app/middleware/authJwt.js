9d54566889c138842135da32d7499cee9f8a2696
rae bautista <cailynrae317@gmail.com>
2024-05-05 14:55:53 +0800

    Minor edit of roles to role const

--- backend/app/middleware/authJwt.js
+++ backend/app/middleware/authJwt.js
@@ -25,7 +25,7 @@ isAdmin = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
 
-    const roles = await Role.find({ _id: { $in: user.roles } });
+    const roles = await Role.find({ _id: { $in: user.role } });
 
     for (let i = 0; i < roles.length; i++) {
       if (roles[i].name === "admin") {
@@ -44,7 +44,7 @@ isMedicalPersonnel = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
 
-    const roles = await Role.find({ _id: { $in: user.roles } });
+    const roles = await Role.find({ _id: { $in: user.role } });
 
     for (let i = 0; i < roles.length; i++) {
       if (roles[i].name === "medical_personnel") {
