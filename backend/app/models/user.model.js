bab05113f92d1c724b0b6f74d5b6f7b1c26f2e32
rae bautista <cailynrae317@gmail.com>
2024-05-05 14:57:16 +0800

    Remove files field in user model

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -1,37 +1,29 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "users",
-  new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true,
-      },
-      birthday: {
-        type: Date,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-    roles: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role"
-      },
-    files: [
-        {
-          filename: String,
-          fileId: mongoose.Schema.Types.ObjectId,
-        },
-      ],
-  },
-)
-);
+const userSchema = new mongoose.Schema({
+  fullname: {
+      type: String,
+      required: true,
+    },
+  birthday: {
+      type: Date,
+      required: true,
+    },
+  email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  password: {
+      type: String,
+      required: true,
+    },
+  role: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role"
+    }
+}, { discriminatorKey: 'kind' }); // Add discriminator key
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
