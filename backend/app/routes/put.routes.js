f4f51f81c397164da8c41f0f36f8511aff3276e0
rae bautista <cailynrae317@gmail.com>
2024-05-06 21:15:58 +0800

    add routes for cancelling appointments and for edit doctor info

--- backend/app/routes/put.routes.js
+++ backend/app/routes/put.routes.js
@@ -1,4 +1,15 @@
 const schedController = require("../controllers/schedule.controller");
+const appointController = require("../controllers/appointment.controller");
+const controller = require("../controllers/auth.controller");
+const { authJwt } = require("../middleware");
+
 module.exports = function(app) {
     app.put("/api/schedule/edit/:scheduleId", schedController.editSchedule);
 };
+
+module.exports = function(app) {
+    app.put("/api/appointments/cancel/:appointmentId", appointController.cancelAppointment);
+};
+module.exports = function(app) {
+    app.put('/api/med/editmed/:medId', authJwt.verifyToken, controller.editMed);
+}
